f370e809d81925ae10e08dfe923d74e6f32e2f3f
Bob Rose <rr637@njit.edu>
2018-10-21 23:37:31 -0400

    Changes to the display files. Content not working.

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class = "container">
     <p class = "float-right"><a href="#">Back to </a></p>
-    <p>$copy; 2017-2018 Learana, Inc. <a href ="#">Privacy</a> <a href=#">Terms</a></p>
+    <p>Robert Rose - IS601 - Laravel Assignment 1 <a href ="#">Placeholder</a> <a href=#">Placeholder</a></p>
 </footer>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.default')
 @section('content')
+
     <h1> Welcome to Assignment IS601b</h1>
 
 
