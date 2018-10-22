aa8224cdf701a75cf3f891d4c3c82aa92a3c4021
Bob Rose <rr637@njit.edu>
2018-10-21 23:20:56 -0400

    Header Changes

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class = "container">
     <p class = "float-right"><a href="#">Back to </a></p>
-    <p>$copy; 2017-2018 Learana, Inc. $middot; <a href ="#">Privacy</a>$middot; <a href=#">Terms</a></p>
+    <p>$copy; 2017-2018 Learana, Inc. <a href ="#">Privacy</a> <a href=#">Terms</a></p>
 </footer>
