c17cd764bab08706703ea282cafefa24bcfe970d
Bob Rose <rr637@njit.edu>
2018-10-21 23:15:06 -0400

    jquery version

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -17,7 +17,7 @@
         @include('includes.footer')
     </footer>
 </main>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 <script src="https://bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </body>
